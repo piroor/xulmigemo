1ebb38f3a25bf75c93168a1d77e845c3bd419469
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-02-04 05:25:08 +0000

    メールフォルダの内容の要約をバックグラウンドで更新するようにした
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@1723 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- content/xulmigemo/mailWindowOverlay.js
+++ content/xulmigemo/mailWindowOverlay.js
@@ -1,5 +1,6 @@
-var XMigemoMailService = {
-	handleEvent : function(aEvent)
+var XMigemoMailService = { 
+	 
+	handleEvent : function(aEvent) 
 	{
 		switch (aEvent.type)
 		{
@@ -11,87 +12,57 @@ var XMigemoMailService = {
 				return;
 		}
 	},
-
-	// nsIFolderListener
-	OnItemAdded : function(aParentItem, aItem) {
-		this.clearCacheFor(aParentItem.Value);
-	},
-	OnItemRemoved : function(aParentItem, aItem) {
-		this.clearCacheFor(aParentItem.Value);
-	},
+ 
+	// nsIFolderListener 
+	OnItemAdded : function(aParentItem, aItem) {},
+	OnItemRemoved : function(aParentItem, aItem) {},
 	OnItemPropertyChanged : function(aItem, aProperty, aOld, aNew) {},
 	OnItemIntPropertyChanged : function(aItem, aProperty, aOld, aNew) {},
 	OnItemBoolPropertyChanged : function(aItem, aProperty, aOld, aNew) {},
 	OnItemUnicharPropertyChanged : function(aItem, aProperty, aOld, aNew) {},
 	OnItemPropertyFlagChanged : function(aItem, aProperty, aOld, aNew) {},
-	OnItemEvent : function(aItem, aEvent) {},
-	clearCacheFor : function(aURI) {
-//dump('event onr '+aURI+'\n');
-		if (aURI in cache) {
-			dump('clear cache for '+aURI+'\n');
-			delete cache[aURI];
-		}
+	OnItemEvent : function(aItem, aEvent) {
+		if (aEvent.toString() == 'FolderLoaded')
+			this.getSummary();
 	},
-
-
-	folderContentsCache : {},
-
-	getTextTables : function()
+ 
+	summaries : {}, 
+ 
+	getSummary : function() 
 	{
 		var uri = gDBView.msgFolder.URI;
-		if (uri in this.folderContentsCache)
-			return this.folderContentsCache[uri];
+		if (uri in this.summaries)
+			return this.summaries[uri];
 //dump('make cache for '+uri+'\n');
 
-		var enum = gDBView.db.EnumerateMessages();
-		var msg;
-		var authors = [];
-		var subjects = [];
-		var recipients = [];
-		var cc = [];
-		while (enum.hasMoreElements())
-		{
-			msg = enum.getNext().QueryInterface(Components.interfaces.nsIMsgDBHdr);
-			authors.push(msg.mime2DecodedAuthor);
-			subjects.push(msg.mime2DecodedSubject);
-			recipients.push(msg.mime2DecodedRecipients);
-			cc.push(XMigemoStrUtils.unescapeFromMime(msg.ccList));
-		}
-		this.folderContentsCache[uri] = {
-			authors : authors.join('\n'),
-			subjects : subjects.join('\n'),
-			recipients : recipients.join('\n'),
-			cc : cc.join('\n'),
-		};
-
-		return this.folderContentsCache[uri];
+		this.summaries[uri] = new XMigemoMailFolderSummary(uri);
+		return this.summaries[uri];
 	},
-
-
-	kQuickSearchSubject       : 0,
+ 
+	kQuickSearchSubject       : 0, 
 	kQuickSearchFrom          : 1,
 	kQuickSearchFromOrSubject : 2,
 	kQuickSearchBody          : 3,
 	kQuickSearchRecipient     : 5,
-
-	getSearchTermsList : function()
+ 
+	getSearchTermsList : function() 
 	{
 		var terms = [];
 		if (!XMigemoService.getPref('xulmigemo.mailnews.threadsearch.enabled'))
 			return terms;
 
 		try {
-			var tables = this.getTextTables();
+			var summaries = this.getSummary();
 			var table = [];
 			if (gSearchInput.searchMode == this.kQuickSearchSubject ||
 				gSearchInput.searchMode == this.kQuickSearchFromOrSubject)
-				table.push(tables.subjects);
+				table.push(summaries.subjects);
 			if (gSearchInput.searchMode == this.kQuickSearchFrom ||
 				gSearchInput.searchMode == this.kQuickSearchFromOrSubject)
-				table.push(tables.authors);
+				table.push(summaries.authors);
 			if (gSearchInput.searchMode == this.kQuickSearchRecipient) {
-				table.push(tables.recipients);
-				table.push(tables.cc);
+				table.push(summaries.recipients);
+				table.push(summaries.cc);
 			}
 			if (table.length) {
 				table = table.join('\n');
@@ -109,13 +80,14 @@ var XMigemoMailService = {
 
 		return terms;
 	},
-
-	init : function() {
+ 
+	init : function() 
+	{
 		window.removeEventListener('load', this, false);
 
 		var mailSession = Components.classes[mailSessionContractID].getService(Components.interfaces.nsIMsgMailSession);
 		var nsIFolderListener = Components.interfaces.nsIFolderListener;
-		mailSession.AddFolderListener(this, nsIFolderListener.added | nsIFolderListener.removed);
+		mailSession.AddFolderListener(this, nsIFolderListener.event);
 
 		eval('window.createSearchTerms = '+
 			window.createSearchTerms.toSource().replace(
@@ -127,17 +99,143 @@ var XMigemoMailService = {
 				]]></>
 			)
 		);
-
 	},
-
-	destroy : function() {
+ 
+	destroy : function() 
+	{
 		window.removeEventListener('unload', this, false);
 
 		var mailSession = Components.classes[mailSessionContractID].getService(Components.interfaces.nsIMsgMailSession);
 		mailSession.RemoveFolderListener(this);
+
+		for (var i in this.summaries)
+		{
+			this.summaries[i].destroy();
+		}
 	}
-};
+ 
+}; 
+  
+function XMigemoMailFolderSummary(aFolder) 
+{
+dump('XMigemoMailFolderSummary initialize'+aFolder+'\n');
 
-window.addEventListener('load', XMigemoMailService, false);
+	this.mFolder = aFolder;
+	this.init();
+}
+XMigemoMailFolderSummary.prototype = {
+	delay : 10,
+	 
+	get authors() 
+	{
+		this.parseAllMessages();
+		return this.mAuthors.join('\n');
+	},
+	get subjects()
+	{
+		this.parseAllMessages();
+		return this.mSubjects.join('\n');
+	},
+	get recipients()
+	{
+		this.parseAllMessages();
+		return this.mRecipients.join('\n');
+	},
+	get cc()
+	{
+		this.parseAllMessages();
+		return this.mCc.join('\n');
+	},
+ 
+	mAuthors : [], 
+	mSubjects : [],
+	mRecipients : [],
+	mCc : [],
+ 
+	init : function() 
+	{
+		var mailSession = Components.classes[mailSessionContractID].getService(Components.interfaces.nsIMsgMailSession);
+		var nsIFolderListener = Components.interfaces.nsIFolderListener;
+		mailSession.AddFolderListener(this, nsIFolderListener.added | nsIFolderListener.removed);
+
+		this.mDB = gDBView.db;
+		this.startToBuild();
+	},
+ 
+	destroy : function() 
+	{
+		this.stopToBuild();
+
+		delete this.mFolder;
+		delete this.mDB;
+
+		var mailSession = Components.classes[mailSessionContractID].getService(Components.interfaces.nsIMsgMailSession);
+		mailSession.RemoveFolderListener(this);
+	},
+ 
+	startToBuild : function() 
+	{
+dump('XMigemoMailFolderSummary start to build '+this.mFolder+'\n');
+		this.stopToBuild();
+		this.mAuthors = [];
+		this.mSubjects = [];
+		this.mRecipients = [];
+		this.mCc = [];
+		this.mMessages = this.mDB.EnumerateMessages();
+		this.mTimer = window.setTimeout(this.parseOneMessage, this.delay, this, true);
+	},
+ 
+	stopToBuild : function() 
+	{
+		if (this.mTimer) {
+			window.clearTimeout(this.mTimer);
+			this.mTimer = null;
+		}
+	},
+ 
+	parseOneMessage : function(aSelf, aUseTimer) 
+	{
+		if (!aSelf.mMessages.hasMoreElements()) return false;
+
+		var msg = aSelf.mMessages.getNext().QueryInterface(Components.interfaces.nsIMsgDBHdr);
+		aSelf.mAuthors.push(msg.mime2DecodedAuthor);
+		aSelf.mSubjects.push(msg.mime2DecodedSubject);
+		aSelf.mRecipients.push(msg.mime2DecodedRecipients);
+		aSelf.mCc.push(XMigemoStrUtils.unescapeFromMime(msg.ccList));
+
+dump('parse: '+aSelf.mFolder+' / '+msg.mime2DecodedAuthor+'\n');
+
+		if (aUseTimer) {
+			aSelf.mTimer = window.setTimeout(aSelf.parseOneMessage, aSelf.delay, aSelf, true);
+		}
+
+		return true;
+	},
+ 
+	parseAllMessages : function() 
+	{
+		while (this.parseOneMessage(this)) {}
+dump('XMigemoMailFolderSummary end of build '+this.mFolder+' ('+this.mAuthors.length+')\n');
+	},
+ 	
+	// nsIFolderListener 
+	OnItemAdded : function(aParentItem, aItem) {
+		if (aParentItem.Value == this.mFolder)
+			this.startToBuild();
+	},
+	OnItemRemoved : function(aParentItem, aItem) {
+		if (aParentItem.Value == this.mFolder)
+			this.startToBuild();
+	},
+	OnItemPropertyChanged : function(aItem, aProperty, aOld, aNew) {},
+	OnItemIntPropertyChanged : function(aItem, aProperty, aOld, aNew) {},
+	OnItemBoolPropertyChanged : function(aItem, aProperty, aOld, aNew) {},
+	OnItemUnicharPropertyChanged : function(aItem, aProperty, aOld, aNew) {},
+	OnItemPropertyFlagChanged : function(aItem, aProperty, aOld, aNew) {},
+	OnItemEvent : function(aItem, aEvent) {}
+ 
+}; 
+  
+window.addEventListener('load', XMigemoMailService, false); 
 window.addEventListener('unload', XMigemoMailService, false);
-
+ 
