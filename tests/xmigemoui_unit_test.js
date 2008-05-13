d03144a399aff11838948fa562e62ad796e35f1d
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-05-13 01:18:07 +0000

    Firefox 3でラベルの検出に失敗していたのを修正
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@2486 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ tests/xmigemoui_unit_test.js
@@ -0,0 +1,39 @@
+// 文字列等に非ASCII文字を使う場合は、ファイルのエンコーディングを
+// UTF-8にしてください。
+
+utils.include('common.inc');
+
+var XMigemoUIUnitTest = new TestCase('XMigemoUIのユニットテスト', {runStrategy: 'async'});
+
+XMigemoUIUnitTest.tests = {
+	setUp : function() {
+		yield utils.setUpTestWindow();
+
+		var retVal = utils.addTab(keyEventTest);
+		yield retVal;
+		commonSetUp(retVal);
+		yield wait;
+		assert.isTrue(XMigemoUI.findBarHidden);
+	},
+
+	tearDown : function() {
+		commonTearDown();
+	},
+
+	'プロパティのチェック': function() {
+		assert.isTrue(XMigemoUI.browser);
+		assert.isTrue(XMigemoUI.activeBrowser);
+		assert.isTrue(XMigemoUI.findMigemoBar);
+		assert.isTrue(XMigemoUI.migemoModeBox);
+		assert.isTrue(XMigemoUI.findBar);
+		assert.isTrue(XMigemoUI.findField);
+		assert.isTrue(XMigemoUI.findLabel);
+		assert.isTrue(XMigemoUI.findCaseSensitiveCheck);
+		assert.isTrue(XMigemoUI.findHighlightCheck);
+		assert.isTrue(XMigemoUI.findModeSelector);
+		assert.isTrue(XMigemoUI.timeoutIndicator);
+		assert.isTrue(XMigemoUI.timeoutIndicatorBox);
+		assert.isTrue(win.XMigemoFind);
+		assert.isTrue(win.XMigemoCore);
+	}
+};
