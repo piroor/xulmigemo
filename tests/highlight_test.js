1492121d62077df6e386f84226e1b0afef5d1947
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-05-14 18:38:46 +0000

    テスト修正
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@2507 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/highlight_test.js
+++ tests/highlight_test.js
@@ -7,6 +7,73 @@ utils.include('quickfind.inc');
 var kSCREEN = '__moz_xmigemo-find-highlight-screen';
 var kHIGHLIGHTS = 'descendant::*[@id="__firefox-findbar-search-id" or @class="__mozilla-findbar-search" or @class="__mozilla-findbar-animation"]';
 
+function assert_highlightCheck(aDisabled, aChecked, aMessage) {
+	var check = XMigemoUI.findHighlightCheck;
+	if (aDisabled)
+		assert.True(check.disabled, aMessage);
+	else
+		assert.isFalse(check.disabled, aMessage);
+	if (aChecked)
+		assert.isTrue(check.checked, aMessage);
+	else
+		assert.isFalse(check.checked, aMessage);
+}
+
+function assert_find_found(aTerm, aMessage) {
+	action.inputTextToField(findField, aTerm);
+	yield 1500;
+	assert.notEquals('notfound', findField.getAttribute('status'), aMessage);
+}
+
+function assert_find_notFound(aTerm, aMessage) {
+	action.inputTextToField(findField, aTerm);
+	yield 1500;
+	assert.equals('notfound', findField.getAttribute('status'), aMessage);
+}
+
+function autoHighlightTest(aMode, aOKShort, aOKLong, aNGShort, aNGLong, aOKLongNum) {
+	var message = 'mode is '+aMode;
+
+	XMigemoUI.findMode = XMigemoUI[aMode];
+	XMigemoUI.findCaseSensitiveCheck.checked = false;
+	findField.focus();
+
+	yield utils.doIteration(assert_find_found(aOKShort, message));
+	assert_highlightCheck(false, false, message);
+	yield utils.doIteration(assert_find_found(aOKLong, message));
+	assert_highlightCheck(false, true, message);
+	var xpathResult = content.document.evaluate(
+		kHIGHLIGHTS,
+		content.document,
+		null,
+		XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+		null
+	);
+	assert.equals(aOKLongNum, xpathResult.snapshotLength, message);
+	yield utils.doIteration(assert_find_found(aNGShort, message));
+	assert_highlightCheck(false, false, message);
+	yield utils.doIteration(assert_find_found(aNGLong, message));
+	assert_highlightCheck(false, true, message);
+}
+
+function assert_screenStateForFind(aTerm, aShown) {
+	action.inputTextToField(findField, aTerm);
+	yield 1500;
+	var screen = content.document.getElementById(kSCREEN);
+	assert.isTrue(screen);
+	var box = content.document.getBoxObjectFor(screen);
+	if (aShown) {
+		assert.equals('on', content.document.documentElement.getAttribute(kSCREEN));
+		assert.isTrue(box.width);
+		assert.isTrue(box.height);
+	}
+	else {
+		assert.notEquals('on', content.document.documentElement.getAttribute(kSCREEN));
+		assert.isFalse(box.width);
+		assert.isFalse(box.height);
+	}
+}
+
 var htmlTests = {
 	setUp : function() {
 		yield utils.setUpTestWindow();
@@ -16,6 +83,8 @@ var htmlTests = {
 		commonSetUp(retVal);
 		yield wait;
 		assert.isTrue(XMigemoUI.findBarHidden);
+		XMigemoUI.highlightCheckedAlways = true;
+		XMigemoUI.highlightCheckedAlwaysMinLength = 5;
 	}
 };
 
@@ -28,48 +97,10 @@ var xmlTests = {
 		commonSetUp(retVal);
 		yield wait;
 		assert.isTrue(XMigemoUI.findBarHidden);
+		XMigemoUI.highlightCheckedAlways = true;
+		XMigemoUI.highlightCheckedAlwaysMinLength = 5;
 	}
 };
-
-function autoHighlightTest(aMode, aOKShort, aOKLong, aNGShort, aNGLong, aOKLongNum) {
-	var message = 'mode is '+aMode;
-
-	XMigemoUI.findMode = XMigemoUI[aMode];
-	XMigemoUI.findCaseSensitiveCheck.checked = false;
-	findField.focus();
-
-	action.inputTextToField(findField, aOKShort);
-	yield 1500;
-	assert.notEquals('notfound', findField.getAttribute('status'), message);
-	assert.isFalse(XMigemoUI.findHighlightCheck.disabled, message);
-	assert.isFalse(XMigemoUI.findHighlightCheck.checked, message);
-
-	action.inputTextToField(findField, aOKLong);
-	yield 1500;
-	assert.notEquals('notfound', findField.getAttribute('status'), message);
-	assert.isFalse(XMigemoUI.findHighlightCheck.disabled, message);
-	assert.isTrue(XMigemoUI.findHighlightCheck.checked, message);
-	var xpathResult = content.document.evaluate(
-		kHIGHLIGHTS,
-		content.document,
-		null,
-		XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-		null
-	);
-	assert.equals(aOKLongNum, xpathResult.snapshotLength, message);
-
-	action.inputTextToField(findField, aNGShort);
-	yield 1500;
-	assert.equals('notfound', findField.getAttribute('status'), message);
-	assert.isFalse(XMigemoUI.findHighlightCheck.disabled, message);
-	assert.isFalse(XMigemoUI.findHighlightCheck.checked, message);
-
-	action.inputTextToField(findField, aNGLong);
-	yield 1500;
-	assert.equals('notfound', findField.getAttribute('status'), message);
-	assert.isFalse(XMigemoUI.findHighlightCheck.disabled, message);
-	assert.isTrue(XMigemoUI.findHighlightCheck.checked, message);
-}
 
 var baseTests = {
 	tearDown : function() {
@@ -77,10 +108,6 @@ var baseTests = {
 	},
 
 	'通常の検索で自動ハイライトが正常に動作するかどうか': function() {
-		XMigemoUI.highlightCheckedAlways = true;
-		XMigemoUI.highlightCheckedAlwaysMinLength = 5;
-		var xpathResult;
-
 		gFindBar.openFindBar();
 		yield wait;
 		yield utils.doIteration(autoHighlightTest(
@@ -94,9 +121,6 @@ var baseTests = {
 	},
 
 	'正規表現検索で自動ハイライトが正常に動作するかどうか': function() {
-		XMigemoUI.highlightCheckedAlways = true;
-		XMigemoUI.highlightCheckedAlwaysMinLength = 5;
-
 		gFindBar.openFindBar();
 		yield wait;
 		yield utils.doIteration(autoHighlightTest(
@@ -110,9 +134,6 @@ var baseTests = {
 	},
 
 	'Migemo検索で自動ハイライトが正常に動作するかどうか': function() {
-		XMigemoUI.highlightCheckedAlways = true;
-		XMigemoUI.highlightCheckedAlwaysMinLength = 5;
-
 		gFindBar.openFindBar();
 		yield wait;
 		yield utils.doIteration(autoHighlightTest(
@@ -126,53 +147,18 @@ var baseTests = {
 	},
 
 	'Safari風自動ハイライト': function() {
-		XMigemoUI.highlightCheckedAlways = true;
-		XMigemoUI.highlightCheckedAlwaysMinLength = 5;
 		XMigemoHighlight.strongHighlight = true;
 
 		gFindBar.openFindBar();
 		yield wait;
 		findField.focus();
 
-		var xpathResult;
-
-
 		XMigemoUI.findMode = XMigemoUI.FIND_MODE_NORMAL;
-
-		action.inputTextToField(findField, 'text field');
-		yield 1500;
-		assert.equals('on', content.document.documentElement.getAttribute(kSCREEN));
-		var screen = content.document.getElementById(kSCREEN);
-		assert.isTrue(screen);
-		var box = content.document.getBoxObjectFor(screen);
-		assert.isTrue(box.width);
-		assert.isTrue(box.height);
-
-
-		action.inputTextToField(findField, '');
-		yield 1500;
-		assert.notEquals('on', content.document.documentElement.getAttribute(kSCREEN));
-		box = content.document.getBoxObjectFor(screen);
-		assert.isFalse(box.width);
-		assert.isFalse(box.height);
-
-
+		yield utils.doIteration(assert_screenStateForFind('text field', true));
+		yield utils.doIteration(assert_screenStateForFind('', false));
 		XMigemoUI.findMode = XMigemoUI.FIND_MODE_MIGEMO;
-
-		action.inputTextToField(findField, 'nihongo');
-		yield 1500;
-		assert.equals('on', content.document.documentElement.getAttribute(kSCREEN));
-		box = content.document.getBoxObjectFor(screen);
-		assert.isTrue(box.width);
-		assert.isTrue(box.height);
-
-
-		action.inputTextToField(findField, '');
-		yield 1500;
-		assert.notEquals('on', content.document.documentElement.getAttribute(kSCREEN));
-		box = content.document.getBoxObjectFor(screen);
-		assert.isFalse(box.width);
-		assert.isFalse(box.height);
+		yield utils.doIteration(assert_screenStateForFind('nihongo', false));
+		yield utils.doIteration(assert_screenStateForFind('', false));
 	}
 };
 
@@ -187,6 +173,21 @@ var highlightTestXML = new TestCase('ハイライト表示の基本テスト（X
 highlightTestXML.tests = xmlTests;
 
 
+function fireClickEventOn(aNode, aButton) {
+	var box = content.document.getBoxObjectFor(aNode);
+	action.fireMouseEventOnElement(
+		content.document.getElementById(kSCREEN),
+		{
+			button  : aButton,
+			x       : box.x + 10,
+			y       : box.y + 5,
+			screenX : box.screenX + 10,
+			screenY : box.screenY + 5
+		}
+	);
+	yield 1500;
+}
+
 var highlightAdvancedTest = new TestCase('ハイライト表示時の発展テスト', {runStrategy: 'async'});
 
 highlightAdvancedTest.tests = {
@@ -197,6 +198,8 @@ highlightAdvancedTest.tests = {
 		yield retVal;
 		commonSetUp(retVal);
 		yield wait;
+		XMigemoUI.highlightCheckedAlways = true;
+		XMigemoHighlight.strongHighlight = true;
 	},
 
 	tearDown : function() {
@@ -204,78 +207,43 @@ highlightAdvancedTest.tests = {
 	},
 
 	'スクリーン上でのクリック操作': function() {
-		XMigemoUI.highlightCheckedAlways = true;
 		XMigemoUI.highlightCheckedAlwaysMinLength = 5;
-		XMigemoHighlight.strongHighlight = true;
 
 		gFindBar.openFindBar();
 		yield wait;
 		findField.focus();
-
 
 		XMigemoUI.findMode = XMigemoUI.FIND_MODE_NORMAL;
 
 		action.inputTextToField(findField, 'text field');
 		yield 1500;
 		assert.equals('on', content.document.documentElement.getAttribute(kSCREEN));
-		var screen = content.document.getElementById(kSCREEN);
-		var box = browser.selectedBrowser.boxObject;
-		var click = {
-				button : 0,
-				x : box.x + 10,
-				y : box.y + 10,
-				screenX : box.screenX + 10,
-				screenY : box.screenY + 10
-			};
-		action.fireMouseEventOnElement(screen, click);
-		yield wait;
-		assert.notEquals('on', content.document.documentElement.getAttribute(kSCREEN));
-
-
-		var tabNum = browser.mTabContainer.childNodes.length;
-
+		yield utils.doIteration(fireClickEventOn(browser.selectedBrowser.boxObject, 0));
+		assert.equals('on', content.document.documentElement.getAttribute(kSCREEN));
 
 		XMigemoHighlight.toggleHighlightScreen(true);
 		yield wait;
+
 		var link = content.document.getElementsByTagName('a')[0];
-		var linkBox = content.document.getBoxObjectFor(link);
-		click.button = 1;
-		click.x = linkBox.x + 10;
-		click.y = linkBox.y + 5;
-		click.screenX = linkBox.screenX + 10;
-		click.screenY = linkBox.screenY + 5;
-		action.fireMouseEventOnElement(screen, click);
-		yield 1500;
+		var tabNum = browser.mTabContainer.childNodes.length;
+		yield utils.doIteration(fireClickEventOn(link, 1));
 		assert.equals('on', content.document.documentElement.getAttribute(kSCREEN));
 		assert.equals(tabNum+1, browser.mTabContainer.childNodes.length);
 
-
-		linkBox = content.document.getBoxObjectFor(link);
-		click.button = 0;
-		click.x = linkBox.x + 10;
-		click.y = linkBox.y + 5;
-		click.screenX = linkBox.screenX + 10;
-		click.screenY = linkBox.screenY + 5;
-		action.fireMouseEventOnElement(screen, click);
-		yield 1500;
+		yield utils.doIteration(fireClickEventOn(link, 0));
 		assert.notEquals('on', content.document.documentElement.getAttribute(kSCREEN));
 		assert.matches(/\#link$/, content.location.href);
 	},
 
 	'自動強調表示の最低文字数が0の時、入力文字列がない場合' : function() {
-		XMigemoUI.highlightCheckedAlways = true;
 		XMigemoUI.highlightCheckedAlwaysMinLength = 0;
-		XMigemoHighlight.strongHighlight = true;
 		XMigemoUI.autoStartQuickFind = true;
 
 		var findTerm = 'nihongo';
 		yield utils.doIteration(assert_quickFind_autoStart(findTerm));
-		assert.isTrue(XMigemoUI.findHighlightCheck.checked);
-
 		for (var i = 0, maxi = findTerm.length; i < maxi; i++)
 		{
-			assert.isFalse(XMigemoUI.findHighlightCheck.disabled);
-			assert.isTrue(XMigemoUI.findHighlightCheck.checked);
+			assert_highlightCheck(false, true);
 			action.fireKeyEventOnElement(findField, key_BS);
 			yield wait;
 			assert.equals(XMigemoUI.FIND_MODE_MIGEMO, XMigemoUI.findMode);
