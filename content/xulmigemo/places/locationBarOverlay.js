f2499b14ca531909d0fa4126ba0e24ed3320cb05
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-06-27 19:55:13 +0000

    結果の取得まで
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@2756 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- content/xulmigemo/places/locationBarOverlay.js
+++ content/xulmigemo/places/locationBarOverlay.js
@@ -1,5 +1,6 @@
 var XMigemoLocationBarOverlay = { 
-	get resolversBox()
+	 
+	get resolversBox() 
 	{
 		if (!this._resolversBox)
 			this._resolversBox = document.getElementById('XMigemoLocationBarSearchResolvers');
@@ -7,41 +8,79 @@ var XMigemoLocationBarOverlay = {
 	},
 	_resolversBox : null,
  
-	resolvers : [],
-	 
 	handleEvent : function(aEvent) 
 	{
 		window.removeEventListener('load', this, false);
 	},
-
-	createNewResolver : function()
+ 
+	createNewResolver : function() 
 	{
-		var id = Math.floor(Math.random() * 65000);
-
-		var panel = document.createElement('panel');
-		panel.setAttribute('id', 'hidden-popup-'+id);
-		panel.setAttribute('type', 'autocomplete-richlistbox');
-		panel.setAttribute('hidden', true);
-
-		var textbox = document.createElement('textbox');
-		textbox.setAttribute('id', 'hidden-locationbar-'+id);
-		textbox.setAttribute('type', 'autocomplete');
-		textbox.setAttribute('autocompletesearch', 'history');
-		textbox.setAttribute('autocompletepopup', 'hidden-popup-'+id);
-
-		this.resolversBox.appendChild(panel);
-		this.resolversBox.appendChild(textbox);
-
-		return { textbox : textbox, panel : panel };
-	},
-
-	doSearch : function(aTerm, aResolver)
-	{
-		aResolver.textbox.open = true;
-		aResolver.textbox.mController.startSearch(aTerm);
+		var resolver = new XMigemoLocationBarSearchResolver();
+		this.resolversBox.appendChild(resolver.panel);
+		this.resolversBox.appendChild(resolver.textbox);
+		return resolver;
 	}
  
 }; 
-  
+ 
 window.addEventListener('load', XMigemoLocationBarOverlay, false); 
- 	
+  
+function XMigemoLocationBarSearchResolver() 
+{
+	this.init();
+}
+XMigemoLocationBarSearchResolver.instances = {};
+XMigemoLocationBarSearchResolver.prototype = {
+	 
+	init : function() 
+	{
+		this.id = Math.floor(Math.random() * 65000);
+
+		var panel = document.createElement('panel');
+		panel.setAttribute('id', 'hidden-popup-'+this.id);
+		panel.setAttribute('resolver-id', this.id);
+		panel.setAttribute('type', 'autocomplete-richlistbox');
+		this.panel = panel;
+
+		var textbox = document.createElement('textbox');
+		textbox.setAttribute('id', 'hidden-locationbar-'+this.id);
+		textbox.setAttribute('resolver-id', this.id);
+		textbox.setAttribute('type', 'autocomplete');
+		textbox.setAttribute('autocompletesearch', 'history');
+		textbox.setAttribute('autocompletepopup', 'hidden-popup-'+this.id);
+		textbox.setAttribute('onsearchcomplete', 'XMigemoLocationBarSearchResolver.instances[this.getAttribute("resolver-id")].onSearchComplete();');
+		this.textbox = textbox;
+
+		XMigemoLocationBarSearchResolver.instances[this.id] = this;
+	},
+ 
+	destroy : function() 
+	{
+		this.textbox.mController.startSearch("");
+		this.textbox.open = false;
+		this.panel.parentNode.removeChild(this.panel);
+		this.panel = null;
+		this.textbox.parentNode.removeChild(this.textbox);
+		this.textbox = null;
+		delete XMigemoLocationBarSearchResolver.instances[this.id];
+	},
+ 
+	get listbox() 
+	{
+		return document.getAnonymousNodes(this.panel)[0];
+	},
+ 
+	doSearch : function(aTerm) 
+	{
+		this.textbox.open = true;
+		this.textbox.mController.startSearch(aTerm);
+	},
+ 
+	onSearchComplete : function() 
+	{
+		alert(this.listbox.itemCount);
+		this.destroy();
+	}
+ 
+}; 
+ 	 
