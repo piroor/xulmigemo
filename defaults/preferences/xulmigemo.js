7908de9a901371d0a9ac14451955df622e112737
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-07-01 07:17:01 +0000

    タイマー処理復活
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@2784 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- defaults/preferences/xulmigemo.js
+++ defaults/preferences/xulmigemo.js
@@ -79,6 +79,8 @@ pref("xulmigemo.places.locationBar",    true);
 pref("xulmigemo.places.locationBar.ignoreURI", true);
 pref("xulmigemo.places.locationBar.delay", 250);
 pref("xulmigemo.places.locationBar.minLength", 0);
+pref("xulmigemo.places.locationBar.thread.findTerms", false);
+pref("xulmigemo.places.locationBar.thread.queryRecords", true);
 pref("xulmigemo.places.bookmarksPanel", true);
 pref("xulmigemo.places.historyPanel",   true);
 pref("xulmigemo.places.organizer",      true);
