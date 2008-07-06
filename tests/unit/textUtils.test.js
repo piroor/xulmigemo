a034c1c894a4e399a08d7b1d98a1a4fcad784b98
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-07-06 16:02:34 +0000

    テストを一通り作成
    git-svn-id: https://www.cozmixng.org/repos/piro/xulmigemo/trunk@2845 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/unit/textUtils.test.js
+++ tests/unit/textUtils.test.js
@@ -1,4 +1,5 @@
 var description = 'XMigemoTextUtilsのテスト';
+//var isAsync = true;
 
 utils.include('../../components/pXMigemoTextUtils.js', null, 'Shift_JIS');
 
@@ -49,6 +50,12 @@ function test_brushUpTerms()
 	);
 }
 
+test_range2Text.description = 'range2Text（DOM Rangeの文字列化）'
+test_range2Text.priority = 'never';
+function test_range2Text()
+{
+}
+
 test_sanitize.description = 'sanitize（正規表現のメタキャラクタのエスケープ）';
 function test_sanitize()
 {
@@ -90,9 +97,65 @@ test_getMatchedTermsFromSource.description = 'getMatchedTermsFromSource（正規
 function test_getMatchedTermsFromSource()
 {
 	var source = <![CDATA[
-		英語,日本語,フランス語,ドイツ語,中国語,english,japanese,french,german,chinese
+		英語,日本語,フランス語,ドイツ語,中国語,english,japanese,french,german,chinese,
 		タガログ語,ポーランド語,ハンガリー語,バルタン星語
 	]]>.toString();
-//	assert.arrayEquals(, utils.getMatchedTermsFromSource('/foobar/', source));
+
+	assert.arrayEquals(
+		['語'],
+		utils.getMatchedTermsFromSource('語', source)
+	);
+
+	var expected = '英語,日本語,フランス語,ドイツ語,中国語,タガログ語,ポーランド語,ハンガリー語,バルタン星語'.split(',');
+	expected.sort();
+	var actual = utils.getMatchedTermsFromSource('[^\\s,]+語', source);
+	assert.arrayEquals(
+		expected,
+		actual
+	);
+}
+
+test_getORFindRegExpFromTerms.description = 'getORFindRegExpFromTerms（OR検索用正規表現の生成）';
+function test_getORFindRegExpFromTerms()
+{
+	var terms = '日本語,英語'.split(',');
+
+	var regexp = utils.getORFindRegExpFromTerms(terms);
+	assert.isTrue(utils.isRegExp('/'+regexp+'/'));
+	regexp = new RegExp(regexp, 'gim');
+	assert.pattern('日本語,フランス語,英語', regexp);
+	assert.pattern('フランス語,英語,日本語', regexp);
+	assert.pattern('日本語', regexp);
+}
+
+test_getANDFindRegExpFromTerms.description = 'getANDFindRegExpFromTerms（AND検索用正規表現の生成）';
+function test_getANDFindRegExpFromTerms()
+{
+	var terms = '日本語,英語'.split(',');
+
+	var regexp = utils.getANDFindRegExpFromTerms(terms);
+	assert.isTrue(utils.isRegExp('/'+regexp+'/'));
+	regexp = new RegExp(regexp, 'gim');
+	assert.pattern('日本語,フランス語,英語', regexp);
+	assert.pattern('フランス語,英語,日本語', regexp);
+	assert.notPattern('日本語,フランス語', regexp);
+}
+
+test_getFoundRange.description = 'getFoundRange（フレーム内のヒット箇所の取得）'
+test_getFoundRange.priority = 'never';
+function test_getFoundRange()
+{
+}
+
+test_isRangeOverlap.description = 'isRangeOverlap（DOM Rangeの重なり合いのチェック）'
+test_isRangeOverlap.priority = 'never';
+function test_isRangeOverlap()
+{
+}
+
+test_delayedSelect.description = 'delayedSelect（DOM Rangeの選択）'
+test_delayedSelect.priority = 'never';
+function test_delayedSelect()
+{
 }
 
