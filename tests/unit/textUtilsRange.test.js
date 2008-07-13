dc048070b7af83ca6cb5d404943eaad6bbc0a03d
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-07-13 16:06:29 +0000

    重複を省いた
    git-svn-id: https://www.cozmixng.org/repos/piro/xulmigemo/trunk@2863 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/unit/textUtilsRange.test.js
+++ tests/unit/textUtilsRange.test.js
@@ -18,24 +18,51 @@ function tearDown()
 }
 
 test_isRangeOverlap.description = 'isRangeOverlap（DOM Rangeの重なり合いのチェック）'
-test_isRangeOverlap.priority = 1;
 function test_isRangeOverlap()
 {
 	var range1 = utils.testDocument.createRange();
 	var range2 = utils.testDocument.createRange();
 	var node = utils.testDocument.getElementById('single-row');
 
+	// range1 == range2
 	range1.selectNode(node);
 	range2.selectNode(node);
 	assert.isTrue(textUtils.isRangeOverlap(range1, range2));
 
+	// [range1[range2]]
 	range1.selectNode(node);
 	range2.selectNodeContents(node);
 	assert.isTrue(textUtils.isRangeOverlap(range1, range2));
+	assert.isTrue(textUtils.isRangeOverlap(range2, range1));
 
-	range1.selectNodeContents(node);
-	range2.selectNode(node);
+	// [range1  [range2]  ]
+	range1.selectNode(node);
+	range2.selectNode(node.getElementsByTagName('input')[0]);
 	assert.isTrue(textUtils.isRangeOverlap(range1, range2));
+	assert.isTrue(textUtils.isRangeOverlap(range2, range1));
+
+	// cross
+	range1.selectNode(node.firstChild);
+	range1.setStart(node.firstChild, 5);
+	range1.setEnd(node.firstChild, 15);
+	range2.selectNode(node.firstChild);
+	range1.setStart(node.firstChild, 0);
+	range1.setEnd(node.firstChild, 10);
+	assert.isTrue(textUtils.isRangeOverlap(range1, range2));
+	assert.isTrue(textUtils.isRangeOverlap(range2, range1));
+
+	// corss(2)
+	range1.selectNode(node.firstChild);
+	range1.setStart(node.firstChild, 5);
+	range1.setEnd(node.firstChild, 15);
+	range2.selectNode(node.firstChild);
+	range1.setStart(node.firstChild, 0);
+	range1.setEnd(node.firstChild, 15);
+	assert.isTrue(textUtils.isRangeOverlap(range1, range2));
+	assert.isTrue(textUtils.isRangeOverlap(range2, range1));
+
+	range1.detach();
+	range2.detach();
 }
 
 test_range2Text.description = 'range2Text（DOM Rangeの文字列化）'
@@ -106,8 +133,30 @@ function test_getFoundRange()
 }
 
 test_delayedSelect.description = 'delayedSelect（DOM Rangeの選択）'
-test_delayedSelect.priority = 'never';
 function test_delayedSelect()
 {
+	var range = utils.testDocument.createRange();
+	var link = utils.testDocument.getElementsByTagName('a')[0];
+	var container = link.parentNode;
+	range.selectNodeContents(link);
+	range.setEnd(link.firstChild, 3);
+	var selection = utils.testContent.getSelection();
+	selection.addRange(range);
+	assert.equals('sam', selection.toString());
+	assert.equals(3, container.childNodes.length);
+
+	textUtils.delayedSelect(link, 3, false);
+
+	var text = link.removeChild(link.firstChild);
+	container.insertBefore(text, link);
+	container.removeChild(link);
+	container.normalize();
+	assert.equals(1, container.childNodes.length);
+	yield 100;
+
+	selection = utils.testContent.getSelection();
+	assert.equals('sam', selection.toString());
+
+	range.detach();
 }
 
