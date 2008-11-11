bfb4ea3da4200ec1f0b22d86d1b0a6a7755c80e6
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-11-11 11:19:16 +0000

    キャッシュから取得される正規表現から改行を除去
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@3311 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/unit/xMigemoEngineJa.test.js
+++ tests/unit/xMigemoEngineJa.test.js
@@ -18,4 +18,31 @@ function test_getRegExpFor()
 	assert.equals('\\)|）', engine.getRegExpFor(')'));
 	assert.equals('\\[|［', engine.getRegExpFor('['));
 	assert.equals('\\]|］', engine.getRegExpFor(']'));
+
+	function assertRegExpFor(aExpected, aInput) {
+		var regexp = engine.getRegExpFor(aInput);
+		regexp = new RegExp(regexp, 'i');
+		assert.pattern(aExpected, regexp);
+	}
+	assertRegExpFor('かかく', 'kak');
+	assertRegExpFor('かこ', 'kak');
+	assertRegExpFor('かっか', 'kak');
+	assertRegExpFor('カントク', 'kantoku');
+	assertRegExpFor('かンとく', 'kanntoku');
+	assertRegExpFor('ｶﾝﾄｸ', 'kantoku');
+	assertRegExpFor('なんニン', 'nannnin');
+	assertRegExpFor('ナンニん', 'nannnin');
+	assertRegExpFor('ウぇるカむ', 'werukam');
+	assertRegExpFor('ウぇるカも', 'werukam');
+	assertRegExpFor('ゑるカム', 'werukam');
+	assertRegExpFor('ゑるカみ', 'werukam');
+	assertRegExpFor('werukam', 'werukam');
+	assertRegExpFor('Dão', 'dao');
+	assertRegExpFor('おー', 'o-');
+	assertRegExpFor('ｵｰ', 'o-');
+	assertRegExpFor('オー', 'o-');
+	assertRegExpFor('おｰ', 'o-');
+	assertRegExpFor('お-', 'o-');
+	assertRegExpFor('日本語', 'nihongo');
+	assertRegExpFor('ハロー', 'hello');
 }
