0962dc813bba80f3ae75d86a646b0485bb0f1475
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-11-15 05:23:04 +0000

    findFirstVisibleNodeのテスト
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@3346 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/unit/pXMigemoFind.test.js
+++ tests/unit/pXMigemoFind.test.js
@@ -1,19 +1,37 @@
 utils.include('pXMigemoClasses.inc.js');
 
-var find;
+var findModule;
 
 function setUp()
 {
-	find = new pXMigemoFind();
-	find.init();
+	yield utils.setUpTestWindow();
+	findModule = new pXMigemoFind();
+	findModule.target = utils.getTestWindow().gBrowser;
 }
 
 function tearDown()
 {
-	find.destroy();
-	find = null;
+	findModule.destroy();
+	findModule = null;
+	utils.tearDownTestWindow();
 }
 
 function testFindFirstVisibleNode()
 {
+	var win = utils.getTestWindow();
+	win.resizeTo(500, 500);
+
+	yield utils.addTab(baseURL+'../res/shortPage.html', { selected : true });
+
+	var frame = utils.contentWindow;
+	frame.scrollTo(0, 0);
+	var node = findModule.findFirstVisibleNode(findModule.FIND_DEFAULT, frame);
+	assert.equals(frame.document.getElementsByTagName('P')[0], node);
+
+	yield utils.addTab(baseURL+'../res/longPage.html', { selected : true });
+
+	frame = utils.contentWindow;
+	frame.scrollTo(0, 0);
+	node = findModule.findFirstVisibleNode(findModule.FIND_DEFAULT, frame);
+	assert.equals(frame.document.getElementsByTagName('P')[0], node);
 }
