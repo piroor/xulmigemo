2f94b01f835deed9ba55f89582124c3857be3657
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2008-12-04 16:40:59 +0000

    テスト作成
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@3475 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ tests/functional/containsHiddenMatchTarget.test.js
@@ -0,0 +1,30 @@
+var description = '非表示の要素の内容として正規表現検索にマッチする物があるケース';
+
+utils.include('common.inc.js');
+
+var gTestPageURI = '../res/containsHiddenMatchTarget.html';
+
+function setUp()
+{
+	yield Do(commonSetUp(gTestPageURI));
+	assert.isTrue(XMigemoUI.hidden);
+}
+
+function tearDown()
+{
+	commonTearDown();
+}
+
+function testRegExpFind()
+{
+	gFindBar.openFindBar();
+	yield wait;
+	XMigemoUI.findMode = XMigemoUI.FIND_MODE_REGEXP;
+	field.focus();
+
+	action.inputTextToField(field, 'a|b', false, true);
+	yield wait;
+
+	assert.isNotNull(XMigemoUI.lastFoundRange);
+	assert.equals('a', XMigemoUI.lastFoundRange.toString());
+}
