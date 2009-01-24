50cc5fe51c7982b97a35d085d618221cf154509b
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-01-24 06:03:23 +0000

    syntax error
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@3534 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/functional/highlightAdvanced.test.js
+++ tests/functional/highlightAdvanced.test.js
@@ -29,7 +29,7 @@ function getTabs(aTabBrowser) {
 			null
 		);
 	var array = [];
-	for (var i = 0, maxi = tabs.snapshotLength; i < maxi)
+	for (var i = 0, maxi = tabs.snapshotLength; i < maxi; i++)
 	{
 		array.push(tabs.snapshotItem(i));
 	}
