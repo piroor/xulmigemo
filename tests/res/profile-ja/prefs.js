03793e207573592925426714a13a479dee2a79b8
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-02-03 13:59:15 +0000

    絞り込み条件のSQL文の生成処理のテスト
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@3548 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/res/profile-ja/prefs.js
+++ tests/res/profile-ja/prefs.js
@@ -2,5 +2,11 @@ user_pref("browser.dom.window.dump.enabled", true);
 user_pref("javascript.options.showInConsole", true);
 
 user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnOpen", false);
+user_pref("browser.warnOnQuit", false);
+user_pref("browser.warnOnRestart", false);
+user_pref("general.warnOnAboutConfig", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+
 user_pref("xulmigemo.lang", "ja");
 user_pref("xulmigemo.dicpath-relative", "[ProfD]\\xulmigemodic");
