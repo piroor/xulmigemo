e9c0aec161c8af1f3aa0293c3e07ac82924d5921
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-04-03 14:59:37 +0000

    テスト項目追加
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@4038 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/unit/docShellIterator.test.js
+++ tests/unit/docShellIterator.test.js
@@ -1,5 +1,4 @@
-// 文字列等に非ASCII文字を使う場合は、ファイルのエンコーディングを
-// UTF-8にしてください。
+var description = 'DocShellIteratorのユニットテスト';
 
 utils.include('../../components/pXMigemoFind.js', null, 'Shift_JIS');
 
@@ -46,71 +45,79 @@ assert.focus = function(aIterator, aFrame) {
 	assert.isTrue(aIterator.isFindable);
 }
 
-var DocShellIteratorTest = new TestCase('DocShellIteratorのユニットテスト');
 
-DocShellIteratorTest.tests = {
-	setUp : function() {
-		yield Do(utils.loadURI('../res/frameTest.html'));
-	},
+function setUp()
+{
+	yield Do(utils.loadURI('../res/frameTest.html'));
+}
 
-	tearDown : function() {
-		iterator.destroy();
-		yield Do(utils.loadURI());
-	},
+function tearDown()
+{
+	iterator.destroy();
+	yield Do(utils.loadURI());
+}
 
-	'内部メソッド': function() {
-		var contentDocShell = getDocShellFromFrame(content);
-		var frame1DocShell = getDocShellFromFrame(content.frames[0]);
-		var frame2DocShell = getDocShellFromFrame(content.frames[1]);
+function testInternalMethods()
+{
+	var contentDocShell = getDocShellFromFrame(content);
+	var frame1DocShell = getDocShellFromFrame(content.frames[0]);
+	var frame2DocShell = getDocShellFromFrame(content.frames[1]);
 
-		iterator = new DocShellIterator(content, false);
-		assert.equals(-1, iterator.getChildOffsetFromDocShellNode(contentDocShell));
-		assert.docShellEquals(contentDocShell, iterator.getDocShellFromFrame(content));
-		assert.docShellEquals(frame1DocShell, iterator.getNextDocShell(contentDocShell));
-		assert.isNull(iterator.getPrevDocShell(contentDocShell));
-		assert.docShellEquals(frame2DocShell, iterator.getLastChildDocShell(contentDocShell));
-		assert.equals(content.frames[0].document.body, iterator.getDocumentBody(content.frames[0].document));
-		iterator.destroy();
+	iterator = new DocShellIterator(content, false);
+	assert.equals(-1, iterator.getChildOffsetFromDocShellNode(contentDocShell));
+	assert.docShellEquals(contentDocShell, iterator.getDocShellFromFrame(content));
+	assert.docShellEquals(frame1DocShell, iterator.getNextDocShell(contentDocShell));
+	assert.isNull(iterator.getPrevDocShell(contentDocShell));
+	assert.docShellEquals(frame2DocShell, iterator.getLastChildDocShell(contentDocShell));
+	assert.equals(content.frames[0].document.body, iterator.getDocumentBody(content.frames[0].document));
+	iterator.destroy();
 
-		iterator = new DocShellIterator(content.frames[0], false);
-		assert.equals(0, iterator.getChildOffsetFromDocShellNode(frame1DocShell));
-		assert.docShellEquals(frame2DocShell, iterator.getNextDocShell(frame1DocShell));
-		assert.docShellEquals(contentDocShell, iterator.getPrevDocShell(frame1DocShell));
-		assert.isNull(iterator.getLastChildDocShell(frame1DocShell));
-		iterator.destroy();
+	iterator = new DocShellIterator(content.frames[0], false);
+	assert.equals(0, iterator.getChildOffsetFromDocShellNode(frame1DocShell));
+	assert.docShellEquals(frame2DocShell, iterator.getNextDocShell(frame1DocShell));
+	assert.docShellEquals(contentDocShell, iterator.getPrevDocShell(frame1DocShell));
+	assert.isNull(iterator.getLastChildDocShell(frame1DocShell));
+	iterator.destroy();
 
-		iterator = new DocShellIterator(content.frames[1], false);
-		assert.equals(1, iterator.getChildOffsetFromDocShellNode(frame2DocShell));
-		assert.isNull(iterator.getNextDocShell(frame2DocShell));
-		assert.docShellEquals(frame1DocShell, iterator.getPrevDocShell(frame2DocShell));
-		iterator.destroy();
-	},
+	iterator = new DocShellIterator(content.frames[1], false);
+	assert.equals(1, iterator.getChildOffsetFromDocShellNode(frame2DocShell));
+	assert.isNull(iterator.getNextDocShell(frame2DocShell));
+	assert.docShellEquals(frame1DocShell, iterator.getPrevDocShell(frame2DocShell));
+	iterator.destroy();
+}
 
-	'前方検索': function() {
-		iterator = new DocShellIterator(content.frames[0], false);
-		assert.initialized(iterator, content.frames[0]);
+function testForward()
+{
+	iterator = new DocShellIterator(content.frames[0], false);
+	assert.initialized(iterator, content.frames[0]);
 
-		assert.isTrue(iterator.iterateNext());
-		assert.focus(iterator, content.frames[1]);
-		assert.isFalse(iterator.isInitial);
-		assert.isTrue(iterator.iterateNext());
-		assert.focus(iterator, content);
-		assert.isFalse(iterator.isInitial);
-		assert.isTrue(iterator.iterateNext());
-		assert.focus(iterator, content.frames[0]);
-	},
+	assert.isTrue(iterator.iterateNext());
+	assert.isFalse(iterator.wrapped);
+	assert.focus(iterator, content.frames[1]);
+	assert.isFalse(iterator.isInitial);
+	assert.isTrue(iterator.iterateNext());
+	assert.isTrue(iterator.wrapped);
+	assert.focus(iterator, content);
+	assert.isFalse(iterator.isInitial);
+	assert.isTrue(iterator.iterateNext());
+	assert.isFalse(iterator.wrapped);
+	assert.focus(iterator, content.frames[0]);
+}
 
-	'後方検索': function() {
-		iterator = new DocShellIterator(content.frames[0], true);
-		assert.initialized(iterator, content.frames[0]);
+function testBackward()
+{
+	iterator = new DocShellIterator(content.frames[0], true);
+	assert.initialized(iterator, content.frames[0]);
 
-		assert.isTrue(iterator.iterateNext());
-		assert.focus(iterator, content);
-		assert.isFalse(iterator.isInitial);
-		assert.isTrue(iterator.iterateNext());
-		assert.focus(iterator, content.frames[1]);
-		assert.isFalse(iterator.isInitial);
-		assert.isTrue(iterator.iterateNext());
-		assert.focus(iterator, content.frames[0]);
-	}
-};
+	assert.isTrue(iterator.iterateNext());
+	assert.isFalse(iterator.wrapped);
+	assert.focus(iterator, content);
+	assert.isFalse(iterator.isInitial);
+	assert.isTrue(iterator.iterateNext());
+	assert.isTrue(iterator.wrapped);
+	assert.focus(iterator, content.frames[1]);
+	assert.isFalse(iterator.isInitial);
+	assert.isTrue(iterator.iterateNext());
+	assert.isFalse(iterator.wrapped);
+	assert.focus(iterator, content.frames[0]);
+}
