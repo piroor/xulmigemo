4a466e349298e36df334b0a48c9169d309471efc
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-04-20 08:29:42 +0000

    XMigemoUI.doProcessForAllFrames()のテスト
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@4137 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/functional/XMigemoUI.test.js
+++ tests/functional/XMigemoUI.test.js
@@ -31,12 +31,44 @@ function testProperties()
 	assert.isTrue(win.XMigemoCore);
 }
 
-function testClearFocusRingForSingleFrame()
+
+testDoProcessForAllFrames.setUp = function()
+{
+	yield Do(utils.loadURI(baseURL+'../res/frameTest.html'));
+};
+function testDoProcessForAllFrames()
+{
+	var scope = { attrName : '_moz-xmigemo-test-attribute' };
+	XMigemoUI.doProcessForAllFrames(
+		function(aFrame) {
+			aFrame.document.documentElement.setAttribute(this.attrName, true);
+		},
+		scope
+	);
+	assert.isTrue(content.document.documentElement.hasAttribute(scope.attrName));
+	assert.isTrue($('frame1', content).contentDocument.documentElement.hasAttribute(scope.attrName));
+	assert.isTrue($('frame2', content).contentDocument.documentElement.hasAttribute(scope.attrName));
+
+	XMigemoUI.doProcessForAllFrames(
+		function(aFrame) {
+			aFrame.document.documentElement.removeAttribute(this.attrName, true);
+		},
+		scope
+	);
+	assert.isFalse(content.document.documentElement.hasAttribute(scope.attrName));
+	assert.isFalse($('frame1', content).contentDocument.documentElement.hasAttribute(scope.attrName));
+	assert.isFalse($('frame2', content).contentDocument.documentElement.hasAttribute(scope.attrName));
+}
+
+testClearFocusRingForSingleFrame.setUp = function()
 {
 	yield Do(utils.loadURI(baseURL+'../res/keyEventTest.html'));
 	Array.slice(content.document.links).forEach(function(aLink) {
 		aLink.setAttribute(XMigemoUI.kFOCUSED, true);
 	});
+};
+function testClearFocusRingForSingleFrame()
+{
 	var links = $X('/descendant::*[@'+XMigemoUI.kFOCUSED+'="true"]', content.document);
 	assert.equals(2, links.length);
 	XMigemoUI.clearFocusRing();
@@ -44,7 +76,7 @@ function testClearFocusRingForSingleFrame()
 	assert.equals(0, links.length);
 }
 
-function testClearFocusRingForMultipleFrames()
+testClearFocusRingForMultipleFrames.setUp = function()
 {
 	yield Do(utils.loadURI(baseURL+'../res/frameTest.html'));
 	Array.slice($('frame1', content).contentDocument.links).forEach(function(aLink) {
@@ -53,7 +85,9 @@ function testClearFocusRingForMultipleFrames()
 	Array.slice($('frame2', content).contentDocument.links).forEach(function(aLink) {
 		aLink.setAttribute(XMigemoUI.kFOCUSED, true);
 	});
-
+};
+function testClearFocusRingForMultipleFrames()
+{
 	var links;
 
 	links = $X('/descendant::*[@'+XMigemoUI.kFOCUSED+'="true"]', $('frame1', content).contentDocument);
