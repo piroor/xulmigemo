325bff3793a3266bea4f554f608349dcd60d1628
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-05-09 20:26:07 +0000

    正規表現検索のテストを追加
    git-svn-id: https://www.cozmixng.org/repos/piro/xulmigemo/trunk@4382 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/unit/pXMigemoFind.test.js
+++ tests/unit/pXMigemoFind.test.js
@@ -18,31 +18,32 @@ function tearDown()
 }
 
 
+function assertFindAndFound(aBackward, aFindTerm, aFoundTerm)
+{
+	findModule.find(aBackward, aFindTerm, false);
+	assertFound(aFoundTerm);
+}
+
+function assertFound(aTerm)
+{
+	assert.equals(aTerm, findModule.foundRange);
+//	assert.equals(aTerm, findModule.foundRange, inspectDOMNode(findModule.foundRange.commonAncestorContainer.parentNode));
+}
+
+
 function assertFindInSingleFrame()
 {
 	yield Do(utils.loadURI(baseURL+'../res/keyEventTest.html'));
 
-	function assertFound(aTerm) {
-		assert.equals(aTerm, findModule.foundRange);
-	}
+	assertFindAndFound(false, 'nihongo', '日本語');
+	assertFindAndFound(false, 'nihongo', 'にほんご');
+	assertFindAndFound(false, 'nihongo', 'ニホンゴ');
+	assertFindAndFound(false, 'nihongo', 'nihongo');
+	assertFindAndFound(false, 'nihongo', '日本語');
 
-	findModule.find(false, 'nihongo', false);
-	assertFound('日本語');
-	findModule.find(false, 'nihongo', false);
-	assertFound('にほんご');
-	findModule.find(false, 'nihongo', false);
-	assertFound('ニホンゴ');
-	findModule.find(false, 'nihongo', false);
-	assertFound('nihongo');
-	findModule.find(false, 'nihongo', false);
-	assertFound('日本語');
-
-	findModule.find(true, 'nihongo', false);
-	assertFound('nihongo');
-	findModule.find(true, 'nihongo', false);
-	assertFound('ニホンゴ');
-	findModule.find(true, 'nihongo', false);
-	assertFound('にほんご');
+	assertFindAndFound(true, 'nihongo', 'nihongo');
+	assertFindAndFound(true, 'nihongo', 'ニホンゴ');
+	assertFindAndFound(true, 'nihongo', 'にほんご');
 
 	findModule.findNext(false);
 	assertFound('ニホンゴ');
@@ -80,45 +81,35 @@ function assertFindInMultipleFrames()
 {
 	yield Do(utils.loadURI(baseURL+'../res/frameTest.html'));
 
-	function assertFoundInDocument(aTerm, aDocument) {
-		assert.equals(aTerm, findModule.foundRange);
-		assert.equals(aDocument, findModule.foundRange.startContainer.ownerDocument);
+	function assertFindAndFoundInDocument(aBackward, aFindTerm, aFoundTerm, aFoundDocument) {
+		findModule.find(aBackward, aFindTerm, false);
+		assertFoundInDocument(aFoundTerm, aFoundDocument);
+	}
+
+	function assertFoundInDocument(aFoundTerm, aFoundDocument) {
+		assert.equals(aFoundTerm, findModule.foundRange);
+		assert.equals(aFoundDocument, findModule.foundRange.startContainer.ownerDocument);
 	}
 
 	var firstDoc = $('frame1', content).contentDocument;
 	var secondDoc = $('frame2', content).contentDocument;
 
-	findModule.find(false, 'nihongo', false);
-	assertFoundInDocument('日本語', firstDoc);
-	findModule.find(false, 'nihongo', false);
-	assertFoundInDocument('にほんご', firstDoc);
-	findModule.find(false, 'nihongo', false);
-	assertFoundInDocument('ニホンゴ', firstDoc);
-	findModule.find(false, 'nihongo', false);
-	assertFoundInDocument('nihongo', firstDoc);
-	findModule.find(false, 'nihongo', false);
-	assertFoundInDocument('日本語', secondDoc);
-	findModule.find(false, 'nihongo', false);
-	assertFoundInDocument('にほんご', secondDoc);
-	findModule.find(false, 'nihongo', false);
-	assertFoundInDocument('ニホンゴ', secondDoc);
-	findModule.find(false, 'nihongo', false);
-	assertFoundInDocument('nihongo', secondDoc);
-	findModule.find(false, 'nihongo', false);
-	assertFoundInDocument('日本語', firstDoc);
+	assertFindAndFoundInDocument(false, 'nihongo', '日本語', firstDoc);
+	assertFindAndFoundInDocument(false, 'nihongo', 'にほんご', firstDoc);
+	assertFindAndFoundInDocument(false, 'nihongo', 'ニホンゴ', firstDoc);
+	assertFindAndFoundInDocument(false, 'nihongo', 'nihongo', firstDoc);
+	assertFindAndFoundInDocument(false, 'nihongo', '日本語', secondDoc);
+	assertFindAndFoundInDocument(false, 'nihongo', 'にほんご', secondDoc);
+	assertFindAndFoundInDocument(false, 'nihongo', 'ニホンゴ', secondDoc);
+	assertFindAndFoundInDocument(false, 'nihongo', 'nihongo', secondDoc);
+	assertFindAndFoundInDocument(false, 'nihongo', '日本語', firstDoc);
 
-	findModule.find(true, 'nihongo', false);
-	assertFoundInDocument('nihongo', secondDoc);
-	findModule.find(true, 'nihongo', false);
-	assertFoundInDocument('ニホンゴ', secondDoc);
-	findModule.find(true, 'nihongo', false);
-	assertFoundInDocument('にほんご', secondDoc);
-	findModule.find(true, 'nihongo', false);
-	assertFoundInDocument('日本語', secondDoc);
-	findModule.find(true, 'nihongo', false);
-	assertFoundInDocument('nihongo', firstDoc);
-	findModule.find(true, 'nihongo', false);
-	assertFoundInDocument('ニホンゴ', firstDoc);
+	assertFindAndFoundInDocument(true, 'nihongo', 'nihongo', secondDoc);
+	assertFindAndFoundInDocument(true, 'nihongo', 'ニホンゴ', secondDoc);
+	assertFindAndFoundInDocument(true, 'nihongo', 'にほんご', secondDoc);
+	assertFindAndFoundInDocument(true, 'nihongo', '日本語', secondDoc);
+	assertFindAndFoundInDocument(true, 'nihongo', 'nihongo', firstDoc);
+	assertFindAndFoundInDocument(true, 'nihongo', 'ニホンゴ', firstDoc);
 
 	findModule.findNext(false);
 	assertFoundInDocument('nihongo', firstDoc);
@@ -192,8 +183,7 @@ function testFindFirstVisibleNode()
 }
 
 
-testGetParentLinkFromRange.setUp = function()
-{
+testGetParentLinkFromRange.setUp = function() {
 	yield Do(utils.loadURI(baseURL+'../res/keyEventTest.html'));
 };
 function testGetParentLinkFromRange()
@@ -210,4 +200,105 @@ function testGetParentLinkFromRange()
 	range.setEnd(textNode, 3);
 	assert.equals('hi', range.toString());
 	assert.isNull(findModule.getParentLinkFromRange(range));
+}
+
+
+testRegExpFind.setUp = function() {
+	findModule.findMode = findModule.FIND_MODE_REGEXP;
+	yield Do(utils.loadURI(baseURL+'../res/caseSensitive.html'));
+};
+function testRegExpFind()
+{
+	findModule.caseSensitive = false;
+
+	assertFindAndFound(false, 'firefox|addon', 'firefox');
+	assertFindAndFound(false, 'firefox|addon', 'Firefox');
+	assertFindAndFound(false, 'firefox|addon', 'FireFox');
+	assertFindAndFound(false, 'firefox|addon', 'addon');
+	assertFindAndFound(false, 'firefox|addon', 'AddOn');
+
+	assertFindAndFound(true, 'firefox|addon', 'addon');
+	assertFindAndFound(true, 'firefox|addon', 'FireFox');
+	assertFindAndFound(true, 'firefox|addon', 'Firefox');
+
+	findModule.findNext(false);
+	assertFound('FireFox');
+	findModule.findNext(false);
+	assertFound('addon');
+	findModule.findNext(false);
+	assertFound('AddOn');
+
+	findModule.findPrevious(false);
+	assertFound('addon');
+	findModule.findPrevious(false);
+	assertFound('FireFox');
+	findModule.findPrevious(false);
+	assertFound('Firefox');
+
+	findModule.caseSensitive = true;
+
+	assertFindAndFound(false, 'firefox|addon', 'addon');
+	assertFindAndFound(false, 'firefox|addon', 'firefox');
+	assertFindAndFound(false, 'firefox|addon', 'addon');
+
+	assertFindAndFound(true, 'firefox|addon', 'firefox');
+	assertFindAndFound(true, 'firefox|addon', 'addon');
+	assertFindAndFound(true, 'firefox|addon', 'firefox');
+
+	findModule.findNext(false);
+	assertFound('addon');
+	findModule.findNext(false);
+	assertFound('firefox');
+	findModule.findNext(false);
+	assertFound('addon');
+
+	findModule.findPrevious(false);
+	assertFound('firefox');
+	findModule.findPrevious(false);
+	assertFound('addon');
+	findModule.findPrevious(false);
+	assertFound('firefox');
+}
+
+
+testRegExpFindWithFlags.setUp = function() {
+	findModule.findMode = findModule.FIND_MODE_REGEXP;
+	findModule.caseSensitive = false;
+	yield Do(utils.loadURI(baseURL+'../res/caseSensitive.html'));
+};
+function testRegExpFindWithFlags()
+{
+	assertFindAndFound(false, '/firefox|addon/', 'firefox');
+	assertFindAndFound(false, '/firefox|addon/', 'addon');
+	assertFindAndFound(false, '/firefox|addon/i', 'firefox');
+	assertFindAndFound(false, '/firefox|addon/i', 'Firefox');
+
+	assertFindAndFound(true, '/firefox|addon/', 'addon');
+	assertFindAndFound(true, '/firefox|addon/', 'firefox');
+	assertFindAndFound(true, '/firefox|addon/i', 'Addon');
+	assertFindAndFound(true, '/firefox|addon/i', 'AddOn');
+
+	assertFindAndFound(false, '/firefox|addon/', 'firefox');
+	findModule.findNext(false);
+	assertFound('addon');
+	findModule.findNext(false);
+	assertFound('firefox');
+
+	assertFindAndFound(false, '/firefox|addon/i', 'firefox');
+	findModule.findNext(false);
+	assertFound('Firefox');
+	findModule.findNext(false);
+	assertFound('FireFox');
+
+	assertFindAndFound(false, '/firefox|addon/', 'firefox');
+	findModule.findPrevious(false);
+	assertFound('addon');
+	findModule.findPrevious(false);
+	assertFound('firefox');
+
+	assertFindAndFound(false, '/firefox|addon/i', 'firefox');
+	findModule.findPrevious(false);
+	assertFound('Addon');
+	findModule.findPrevious(false);
+	assertFound('AddOn');
 }
