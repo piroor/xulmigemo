f569f4d5332db1610157d713f0f2db8b2da9558c
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2010-03-23 06:07:20 +0000

    update buildscript
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@6355 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- make.sh
+++ make.sh
@@ -5,10 +5,10 @@ appname=xulmigemo
 cp buildscript/make_new.sh ./
 
 ./xpidl.sh xulrunner-sdk-1.9.2
-./make_new.sh -n $appname -v 0 -suffix mozilla-1.9.2
+./make_new.sh -n $appname -v 0 -s mozilla-1.9.2
 
 ./xpidl.sh xulrunner-sdk-central
-./make_new.sh -n $appname -v 0 -suffix mozilla-central
+./make_new.sh -n $appname -v 0 -s mozilla-central
 
 rm ./make_new.sh
 
