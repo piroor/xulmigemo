53fd051170952a4dbb338be4944c3f75053f431a
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2010-03-23 07:06:20 +0000

    makexpi
    git-svn-id: http://www.cozmixng.org/repos/piro/xulmigemo/trunk@6357 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- make.bat
+++ make.bat
@@ -1,9 +1,9 @@
-copy buildscript\make_new.sh .\
+copy buildscript\makexpi.sh .\
 
 call xpidl.bat xulrunner-sdk-1.9.2
-bash make_new.sh -n xulmigemo -v 0 -s "1.9.2"
+bash makexpi.sh -n xulmigemo -v 0 -s "1.9.2"
 
 call xpidl.bat xulrunner-sdk-central
-bash make_new.sh -n xulmigemo -v 0 -s "central"
+bash makexpi.sh -n xulmigemo -v 0 -s "central"
 
-del make_new.sh
+del makexpi.sh
--- make.sh
+++ make.sh
@@ -2,13 +2,13 @@
 
 appname=xulmigemo
 
-cp buildscript/make_new.sh ./
+cp buildscript/makexpi.sh ./
 
 ./xpidl.sh xulrunner-sdk-1.9.2
-./make_new.sh -n $appname -v 0 -s mozilla-1.9.2
+./makexpi.sh -n $appname -v 0 -s mozilla-1.9.2
 
 ./xpidl.sh xulrunner-sdk-central
-./make_new.sh -n $appname -v 0 -s mozilla-central
+./makexpi.sh -n $appname -v 0 -s mozilla-central
 
-rm ./make_new.sh
+rm ./makexpi.sh
 
