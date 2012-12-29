40eca4f4ae042c3580ab75cfd17b99aaf25722bb
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2012-12-29 19:53:14 +0900

    Create XPI without jar file

--- make.bat
+++ make.bat
@@ -1,10 +1,10 @@
 copy buildscript\makexpi.sh .\
 
 call xpidl.bat xulrunner-sdk-1.9.2
-bash makexpi.sh -n xulmigemo -v 0
+bash makexpi.sh -n xulmigemo -v 0 -o
 rem bash makexpi.sh -n xulmigemo -v 0 -s "1.9.2"
 
 rem call xpidl.bat xulrunner-sdk-central
-rem bash makexpi.sh -n xulmigemo -v 0 -s "central"
+rem bash makexpi.sh -n xulmigemo -v 0 -o -s "central"
 
 del makexpi.sh
