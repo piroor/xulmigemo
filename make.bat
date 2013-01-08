d28a71b557c83c8ebab03c5e3b1085ac940a5061
YUKI Hiroshi <shimoda@clear-code.com>
2013-01-08 19:53:35 +0900

    Fix options for makexpi.sh

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 copy buildscript\makexpi.sh .\
 
 call xpidl.bat xulrunner-sdk-1.9.2
-bash makexpi.sh -n xulmigemo -v 0 -o
+bash makexpi.sh -n xulmigemo -o
 rem bash makexpi.sh -n xulmigemo -v 0 -s "1.9.2"
 
 rem call xpidl.bat xulrunner-sdk-central
--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ appname=xulmigemo
 cp buildscript/makexpi.sh ./
 
 ./xpidl.sh xulrunner-sdk-1.9.2
-./makexpi.sh -n $appname -v 0 -o
+./makexpi.sh -n $appname -o
 # ./makexpi.sh -n $appname -v 0 -o -s mozilla-1.9.2
 
 # ./xpidl.sh xulrunner-sdk-central
