d3befe8a4b16a45805211f1f41c6d41bc3d63fda
YUKI Hiroshi <shimoda@clear-code.com>
2016-05-12 18:33:59 +0900

    Don't register XPCOM components

--- make.bat
+++ make.bat
@@ -1,10 +1,5 @@
 copy makexpi\makexpi.sh .\
 
-call xpidl.bat xulrunner-sdk-1.9.2
 bash makexpi.sh -n xulmigemo -o
-rem bash makexpi.sh -n xulmigemo -v 0 -s "1.9.2"
-
-rem call xpidl.bat xulrunner-sdk-central
-rem bash makexpi.sh -n xulmigemo -v 0 -o -s "central"
 
 del makexpi.sh
--- make.sh
+++ make.sh
@@ -4,12 +4,7 @@ appname=xulmigemo
 
 cp makexpi/makexpi.sh ./
 
-./xpidl.sh xulrunner-sdk-1.9.2
 ./makexpi.sh -n $appname -o
-# ./makexpi.sh -n $appname -v 0 -o -s mozilla-1.9.2
-
-# ./xpidl.sh xulrunner-sdk-central
-# ./makexpi.sh -n $appname -v 0 -o -s mozilla-central
 
 rm ./makexpi.sh
 
