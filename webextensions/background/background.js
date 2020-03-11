0a2b2a06954ebf367813e2d09aa05e0895a0ba90
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-03-11 11:30:19 +0900

    Try to find histories and bookmarks for each term in parallel

--- webextensions/background/background.js
+++ webextensions/background/background.js
@@ -7,10 +7,31 @@
 
 browser.omnibox.onInputChanged.addListener(async (text, suggest) => {
   console.log('onInputChanged: ', text);
-  const items = await browser.history.search({ text });
-  suggest(items.map(item => ({
-    content:     item.url,
-    description: item.title
+  const terms = text.trim().split(/\s+/);
+  const items = (await Promise.all(terms.map(async term => {
+    return (await Promise.all([
+      browser.history.search({
+        text: term
+      }).catch(error => {
+        console.error(error);
+        return [];
+      }),
+      browser.bookmarks.search({
+        query: term
+      }).catch(error => {
+        console.error(error);
+        return [];
+      })
+    ])).flat();
+  }))).flat();
+  console.log('items => ', items);
+  const uniqueItems = Array.from(
+    new Set(items.map(item => `${item.url}\n${item.title}`)),
+    item => item.split('\n')
+  );
+  suggest(uniqueItems.map(item => ({
+    content:     item[0],
+    description: item[1]
   })));
 });
 
