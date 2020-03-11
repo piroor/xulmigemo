e76e6aeae0ba84b36692f56f93a05f163c727331
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-03-11 12:59:34 +0900

    Load dictionary onload

--- webextensions/common/common.js
+++ webextensions/common/common.js
@@ -8,7 +8,34 @@
 import Configs from '/extlib/Configs.js';
 
 export const configs = new Configs({
+  dicKeys: [
+    'ja/alpha2',
+
+    'ja/a2',
+    'ja/ka2',
+    'ja/sa2',
+    'ja/ta2',
+    'ja/na2',
+    'ja/ha2',
+    'ja/ma2',
+    'ja/ya2',
+    'ja/ra2',
+    'ja/wa2',
+
+    'ja/ga2',
+    'ja/za2',
+    'ja/da2',
+    'ja/ba2',
+    'ja/pa2'
+  ],
+  dic: {},
+  cacheKeys: [
+    'ja/ja.cache'
+  ],
+  cache: {}
 }, {
   localKeys: [
+    'dic',
+    'cache'
   ]
 });
