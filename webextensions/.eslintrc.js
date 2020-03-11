41865a5923b10b95df75ef21cb66456993de10e9
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-03-11 10:51:01 +0900

    Add skeleton

---
+++ webextensions/.eslintrc.js
@@ -0,0 +1,44 @@
+/*eslint-env commonjs*/
+/*eslint quote-props: ['error', "always"] */
+
+module.exports = {
+  'root': true,
+
+  'parserOptions': {
+    'ecmaVersion': 2017,
+  },
+
+  'env': {
+    'browser': true,
+    'es6': true,
+    'webextensions': true,
+  },
+
+  'settings': {
+    'import/resolver': {
+      'babel-module': {
+        'root': ['./'],
+      }
+    }
+  },
+
+  'rules': {
+    // stylisitc problem
+    'indent': ['warn', 2, {
+      'SwitchCase': 1,
+      'MemberExpression': 1,
+      'CallExpression': {
+        'arguments': 'first',
+      },
+      'VariableDeclarator': {
+        'var': 2,
+        'let': 2,
+        'const': 3,
+      }
+    }],
+    'quotes': ['warn', 'single', {
+      'avoidEscape': true,
+      'allowTemplateLiterals': true,
+    }],
+  }
+};
